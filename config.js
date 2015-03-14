a494d278cc089f94c0d6fdd9fbf1c55587bd4d9d
davidyack <dyack@coloradotc.com>
2015-03-13 19:32:22 -0600

    added themes, customized config

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://crm.davidyack.com',
+        url: 'http://dyblog.azurewebsites.net',
         mail: {},
         database: {
             client: 'sqlite3',
