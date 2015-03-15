d07ccac8df9e117f49f3cf5c77e16ab20ac84175
davidyack <dyack@coloradotc.com>
2015-03-15 00:11:29 -0600

    changed url

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://owhblog.azurewebsites.net',
+        url: 'http://dyphotoblog.azurewebsites.net',
         mail: {},
         database: {
             client: 'sqlite3',
